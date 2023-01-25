ddb7e83eb4e17b5df3ab3ac72ff457fd20c2a062
Oleh Medvid <meandjob@protonmail.com>
2023-01-25 22:48:03 +0200

    edit: data folder

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 require("colors");
-const { news } = require ("./data")
-const { News } = require("./models/news");
 
 const app = require("./app");
 
@@ -13,7 +11,6 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-       News.insertMany(news);
       console.log(
         `Server running. Use our API on port: ${PORT} !!! Database success`.cyan
           .bold.italic
