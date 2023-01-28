c10eafc38531f7bff667057cba6e1f0858f3b9fa
Oleh Medvid <meandjob@protonmail.com>
2023-01-28 21:39:34 +0200

    update: user model

--- models/user.js
+++ models/user.js
@@ -4,27 +4,46 @@ const { handleMongooseError } = require("../helpers");
 
 // eslint-disable-next-line no-useless-escape
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const phoneRegexp = /^[+][(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]$/;
+const nameRegexp = /^[a-zA-Z]+$/;
+const addressRegexp = /^[a-zA-Z]+(,\s[a-zA-Z]+)*$/;
+const pwdRegexp = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{7,32}$/;
+
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
+      match: nameRegexp,
+      required: [true, "Name is required"],
     },
     email: {
       type: String,
       match: emailRegexp,
       unique: true,
-      required: true,
+      required: [true, "Email is required"],
     },
     password: {
       type: String,
-      minlength: 6,
+      match: pwdRegexp,
+      minlength: 7,
       required: true,
     },
-    subscription: {
+    birthday: {
       type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
+      default: null,
+    },
+    phone: {
+      type: String,
+      match: phoneRegexp,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    favorite: {
+      type: Array,
+      default: null,
     },
     avatarURL: {
       type: String,
@@ -49,13 +68,15 @@ const userSchema = new Schema(
 userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().pattern(nameRegexp).required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(7).max(32).pattern(pwdRegexp).required(),
+  address: Joi.string().pattern(addressRegexp).required(),
+  phone: Joi.string().pattern(phoneRegexp).required(),
 });
 const loginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(7).pattern(pwdRegexp).required(),
 });
 
 const verifyEmailSchema = Joi.object({
