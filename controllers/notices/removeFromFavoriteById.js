0a08f7cb8189905c12b274251e663894364d503b
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2023-01-29 21:10:01 +0200

    bugfix

--- controllers/notices/removeFromFavoriteById.js
+++ controllers/notices/removeFromFavoriteById.js
@@ -1,12 +1,15 @@
 const { User } = require("../../models/user");
+const { ObjectId } = require("mongodb");
 
 const removeFromFavoriteById = async (req, res) => {
   const { noticeId } = req.params;
   const { _id } = req.user;
+  const favoriteId = new ObjectId(noticeId);
+  console.log(favoriteId);
 
   const result = await User.updateOne(
     { _id },
-    { $pull: { favorite: noticeId } }
+    { $pull: { favorite: favoriteId } }
   );
   console.log(result);
   if (!result) {
