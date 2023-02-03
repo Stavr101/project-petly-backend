08896ac69dc287845a29a5cf9d8e17944c1859a0
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2023-02-03 19:16:36 +0200

    fixed user index

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,11 +1,13 @@
 const getUser = require("./getUser");
 const addPet = require("./addPet");
 const getAll = require("./getAll");
-const removePet = require("./removePet")
+const removePet = require("./removePet");
+const updateById = require("./updateById");
 
 module.exports = {
-    getUser,
-    addPet,
-    getAll,
-    removePet,
-}
+  getUser,
+  addPet,
+  getAll,
+  removePet,
+  updateById,
+};
