140a4030ceb4abb3f82307c182e14a1278e1993c
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2023-02-04 18:13:26 +0200

    refine code

--- controllers/notices/getFavoriteNotices.js
+++ controllers/notices/getFavoriteNotices.js
@@ -4,7 +4,6 @@ const getFavoriteNotices = async (req, res) => {
   const { favorite } = req.user;
   const { search, page, limit } = req.query;
   const filter = searchFilter({ search, page, limit, favorite });
-  // const result = await Notice.find({ _id: { $in: favorite } });
   const result = await Notice.aggregate(filter);
 
   res.json(result);
